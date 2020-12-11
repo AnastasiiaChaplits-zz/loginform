e9498a70b02685863ad36d94265c64e222d4a6ae
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2020-12-11 17:53:49 +0200

    Refactor route decorator

--- build/controllers/decorators/routes.js
+++ build/controllers/decorators/routes.js
@@ -1,7 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.del = exports.post = exports.put = exports.get = void 0;
+exports.patch = exports.del = exports.post = exports.put = exports.get = void 0;
 require("reflect-metadata");
+var Methods_1 = require("./Methods");
 function routeBinder(method) {
     return function (path) {
         return function (target, key, desc) {
@@ -10,7 +11,8 @@ function routeBinder(method) {
         };
     };
 }
-exports.get = routeBinder('get');
-exports.put = routeBinder('put');
-exports.post = routeBinder('post');
-exports.del = routeBinder('delete');
+exports.get = routeBinder(Methods_1.Methods.get);
+exports.put = routeBinder(Methods_1.Methods.put);
+exports.post = routeBinder(Methods_1.Methods.post);
+exports.del = routeBinder(Methods_1.Methods.del);
+exports.patch = routeBinder(Methods_1.Methods.patch);
